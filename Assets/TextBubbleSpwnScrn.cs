d6eb0ab81c36a3178e928b30203de3b733cc353e
WinaGodwin Anyanwu <w.anyanwu@live.com>
2019-01-26 16:59:54 -0500

    Randomness added to textbubble position

--- Assets/TextBubbleSpwnScrn.cs
+++ Assets/TextBubbleSpwnScrn.cs
@@ -8,13 +8,27 @@ public class TextBubbleSpwnScrn : MonoBehaviour
 {
 	public GameObject txtBubbleScrn;
 	public GameObject panel;
-	public TextContainer panelTxt;
+	public TextMeshProUGUI panelTxt;
 	// Start is called before the first frame update
 	void Start()
 	{
 		RectTransform bubScrnTrns = txtBubbleScrn.GetComponent<RectTransform>();
 		Vector2 bubScrnTopR = bubScrnTrns.anchorMax;
 		Vector2 bubScrnBottomL = bubScrnTrns.anchorMin;
+		Vector2 size = bubScrnTrns.sizeDelta;
+
+		float xPos = UnityEngine.Random.Range(0, size.x);
+		float yPos = UnityEngine.Random.Range(0, size.y);
+		//float xPos = UnityEngine.Random.Range(bubScrnBottomL.x, bubScrnTopR.x);
+		//float yPos = UnityEngine.Random.Range(bubScrnBottomL.y, bubScrnTopR.y);
+
+		panel.GetComponent<RectTransform>().position = new Vector2(xPos, yPos);
+		//panelTxt.GetComponent<RectTransform>().position = panel.GetComponent<RectTransform>().position;
+	}
+
+	private void Update()
+	{
+		panelTxt.text = "this works";
 	}
 
 }
