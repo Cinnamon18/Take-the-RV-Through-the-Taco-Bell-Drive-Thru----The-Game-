5c3d624950edc8333bdba81b8ccf1136c34cf69e
WinaGodwin Anyanwu <w.anyanwu@live.com>
2019-01-26 01:39:42 -0500

    Timer displays minutes properly

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -6,21 +6,31 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
 	public Text timerTxt;
-	private float startTime;
+	public float timeLeft;
    
     void Start()
     {
-		startTime = Time.time;
+		StartCoroutine("CountDown");
+		Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-		float t = Time.time - startTime;
+		float t = timeLeft;
 
 		string min = ((int)t / 60).ToString();
-		string sec = (t % 60).ToString("f2");
+		string sec = (t % 60).ToString("00");
 
 		timerTxt.text = min + ":" + sec;
     }
+
+	IEnumerator CountDown()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds (1);
+			timeLeft--;
+		}
+	}
 }
