7b9a777fa09e9991b7e398fb0ab8f7581d218e90
DESKTOP-NMLP3OO\Sarah Tsai <sarah.y.tsai@gmail.com>
2019-01-26 16:51:01 -0500

    nicer level selection menu

--- Assets/LevelSelection.cs
+++ Assets/LevelSelection.cs
@@ -7,25 +7,47 @@ using UnityEngine.SceneManagement;
 public class LevelSelection : MonoBehaviour
 {
 	public Button levelOneButton;
-
+    public RectTransform rv;
+	public int sceneIndex;
+	public Vector3 target;
+    public bool rvMoving;
+    private int rvSpeed;
 	// Start is called before the first frame update
 	void Start()
     {
+        rvMoving = false;
+        rvSpeed = 3;
 		levelOneButton.onClick.AddListener(delegate { transitionToLevel(1); });
+    }
+
+	void Update()
+    {
+        if (rvMoving)
+        {
+            float step = 400 * Time.deltaTime;
+            rv.position = Vector3.MoveTowards(rv.position, target, step);
+			if (rv.position == target)
+            {
+                SceneManager.LoadScene(sceneIndex);
+            }
+        } else
+        {
+            rv.rotation = Quaternion.Euler(0f, 0f, 15 * Mathf.Sin(Time.time * rvSpeed));
+        }
     }
 
 	void transitionToLevel(int levelNo)
 	{
-		switch (levelNo)
-		{
-			case 1:
-				Debug.Log("Level 1");
-				SceneManager.LoadScene("Level 1");
-				break;
-			default:
-				Debug.Log("Default case");
-				break;
-		}
+        rvMoving = true;
+        sceneIndex = levelNo;
+		switch(levelNo)
+        {
+            case 1:
+                target = levelOneButton.transform.position;
+                break;
+            default:
+                Debug.Log("Default");
+                break;
+        }
 	}
-    
 }
