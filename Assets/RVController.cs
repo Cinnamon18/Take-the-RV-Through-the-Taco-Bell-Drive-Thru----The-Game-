e5fe8228f9af217842be799e43406d2a519558cb
sean-fisher <sean999fisher@gmail.com>
2019-01-25 23:18:33 -0500

    Some RV Controller improvements

--- Assets/RVController.cs
+++ Assets/RVController.cs
@@ -2,8 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class RVController : MonoBehaviour
 {
+
+    public float maxSpeed;
+    public float maxAcceleration;
+    public float turnRate;
+
+    Rigidbody rigidbody;
+    float mass;
+
+    void Awake() {
+        rigidbody = GetComponent<Rigidbody>();
+        mass = rigidbody.mass;
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +28,21 @@ public class RVController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        if (xAxis != 0) {
+
+        }
+        if (yAxis != 0) {
+            Vector3 forwardForce = transform.forward * Time.deltaTime * maxAcceleration * yAxis * mass;
+            Debug.Log(forwardForce);
+            rigidbody.AddForce(forwardForce);
+
+            if (rigidbody.velocity.magnitude > maxSpeed) {
+                rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;// new Vector3();
+            }
+        }
         
     }
 }
