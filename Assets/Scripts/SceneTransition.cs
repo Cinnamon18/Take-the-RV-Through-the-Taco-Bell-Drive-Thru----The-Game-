efd7a5130fe949ff2e70bd26ed6bee53d0135852
WinaGodwin Anyanwu <w.anyanwu@live.com>
2019-01-26 17:34:30 -0500

    Timer starts on scene transition

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,5 +7,6 @@ public static class SceneTransition {
 	public static void LoadScene(string scene) {
 		
 		SceneManager.LoadScene(scene);
+		AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("UI");
 	}
 }
