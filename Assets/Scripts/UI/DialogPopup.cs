da655e610bd9bd89a2e757b89003788a1d030ff5
sean-fisher <sean999fisher@gmail.com>
2019-01-26 18:48:26 -0500

    Improved popup dialog

--- Assets/Scripts/UI/DialogPopup.cs
+++ Assets/Scripts/UI/DialogPopup.cs
@@ -6,16 +6,44 @@ public class DialogPopup : MonoBehaviour
 {
 
     Camera cam;
+    RectTransform rect;
+    public AnimationCurve movementCurve;
+    public float timeToAnimate;
+    Vector2 initialPosition;
+    public Vector2 finalOffset;
+    public float offsetScale;
 
     // Start is called before the first frame update
     void Start()
     {
+        rect = GetComponent<RectTransform>();
         cam = GameObject.FindObjectOfType<Camera>();
+        StartCoroutine(MovingPanelOntoScreen());
+        initialPosition = rect.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.rotation = cam.transform.rotation;
+    }
+
+    IEnumerator MovingPanelOntoScreen() {
+
+        float progress = 0;
+        float step = 1f / timeToAnimate;
+        
+        while(progress < 1) {
+            progress += Time.deltaTime * step;
+
+            
+            rect.localPosition = movementCurve.Evaluate(progress) * finalOffset * offsetScale + initialPosition;
+            rect.localScale = new Vector3(1, 1, 1) * movementCurve.Evaluate(progress);
+            //Vector2.Lerp(initialPosition, initialPosition + finalOffset * movementCurve.Evaluate(progress), 1);
+
+            yield return null;
+        }
+
+        rect.localPosition = initialPosition + movementCurve.Evaluate(1) * finalOffset * offsetScale;
     }
 }
