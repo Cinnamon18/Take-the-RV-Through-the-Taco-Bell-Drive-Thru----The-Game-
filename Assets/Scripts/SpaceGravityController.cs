85ab033bf618089a61a5dbf10c2c10ca9baeffed
sean-fisher <sean999fisher@gmail.com>
2019-01-26 11:12:49 -0500

    Added basic space car stuff

---
+++ Assets/Scripts/SpaceGravityController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SpaceGravityController : MonoBehaviour
+{
+
+    public bool useSpaceGravity;
+    public float gravityStrength;
+    Rigidbody rigidbody;
+
+    void Awake() {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (useSpaceGravity) {
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.up * -1, out hit, 10f)) {
+                // ground found below;
+                Debug.DrawRay(transform.position, transform.up * -1 * hit.distance, Color.yellow);
+                Physics.gravity = hit.normal * -1;
+            }
+
+            //transform.position += Vector3.down * 2 * Time.deltaTime;
+            //rigidbody.AddForce(Vector3.down * rigidbody.mass * gravityStrength);
+        }
+    }
+}
