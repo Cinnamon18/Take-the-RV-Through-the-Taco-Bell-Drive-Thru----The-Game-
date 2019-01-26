f0fb8dfc10b4f597df13863c9ad4d6bbd6ca31a8
Cinnamon18 <alice.ava.moh@gmail.com>
2019-01-26 16:38:23 -0500

    maybe. my collisiosns will finally survive git.

--- Assets/Scripts/RVController.cs
+++ Assets/Scripts/RVController.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
 using CollisionTypes;
 using System;
 
@@ -15,16 +14,13 @@ public class AxleInfo {
 
 public class RVController : MonoBehaviour {
 
-    bool controlEnabled = true;
+	public List<AxleInfo> axleInfos;
+	public float maxMotorTorque;
+	public float maxSteeringAngle;
 
-    public List<AxleInfo> axleInfos; 
-    public float maxMotorTorque;
-    public float maxSteeringAngle;
+	public float maxWheelSpeed;
+	public bool enableFastStop;
 
-    public float maxWheelSpeed;
-    public bool enableFastStop;
-    public WinPanel winPanel;
-     
 	private CollisionDetector collDetect;
 	[SerializeField]
 	private AnimationCurve drivingBadnessDecay;
@@ -39,64 +35,63 @@ public class RVController : MonoBehaviour {
 		drivingBadness = Math.Max(0, drivingBadness);
 		Debug.Log(drivingBadness);
 	}
-    // finds the corresponding visual wheel
-    // correctly applies the transform
-    public void ApplyLocalPositionToVisuals(WheelCollider collider)
-    {
-        if (collider.transform.childCount == 0) {
-            return;
-        }
-     
-        Transform visualWheel = collider.transform.GetChild(0);
-     
-        Vector3 position;
-        Quaternion rotation;
-        collider.GetWorldPose(out position, out rotation);
-     
-        visualWheel.transform.position = position;
-        visualWheel.transform.rotation = rotation;
-    }
-     
-    public void FixedUpdate()
-    {
-        float xAxis     = controlEnabled ? Input.GetAxis("Horizontal")  : 0;
-        float yAxis     = controlEnabled ? Input.GetAxis("Vertical")    : 0;
-        float steering  = maxSteeringAngle  * xAxis;
-        float motorInput= maxMotorTorque    * yAxis;
-        //float motorInput= Mathf.Min(maxMotorTorque    * yAxis, maxWheelSpeed);
-     
-        foreach (AxleInfo axleInfo in axleInfos) {
-            if (axleInfo.steering) {
-                axleInfo.leftWheel.steerAngle = steering;
-                axleInfo.rightWheel.steerAngle = steering;
-            }
-            if (axleInfo.motor) {
-                float motorVal = motorInput;
+
+	// finds the corresponding visual wheel
+	// correctly applies the transform
+	public void ApplyLocalPositionToVisuals(WheelCollider collider) {
+		if (collider.transform.childCount == 0) {
+			return;
+		}
+
+		Transform visualWheel = collider.transform.GetChild(0);
+
+		Vector3 position;
+		Quaternion rotation;
+		collider.GetWorldPose(out position, out rotation);
+
+		visualWheel.transform.position = position;
+		visualWheel.transform.rotation = rotation;
+	}
+
+	public void FixedUpdate() {
+		float xAxis = Input.GetAxis("Horizontal");
+		float yAxis = Input.GetAxis("Vertical");
+		float steering = maxSteeringAngle * xAxis;
+		float motorInput = maxMotorTorque * yAxis;
+		//float motorInput= Mathf.Min(maxMotorTorque    * yAxis, maxWheelSpeed);
+
+		foreach (AxleInfo axleInfo in axleInfos) {
+			if (axleInfo.steering) {
+				axleInfo.leftWheel.steerAngle = steering;
+				axleInfo.rightWheel.steerAngle = steering;
+			}
+			if (axleInfo.motor) {
+				float motorVal = motorInput;
 
 
-                axleInfo.leftWheel.motorTorque = motorInput;
-                axleInfo.rightWheel.motorTorque = motorInput;
+				axleInfo.leftWheel.motorTorque = motorInput;
+				axleInfo.rightWheel.motorTorque = motorInput;
 
-            }
-            
-            if (enableFastStop) {
-                if (yAxis == 0) {
+			}
 
-                    axleInfo.leftWheel.brakeTorque  = 500000;
-                    axleInfo.rightWheel.brakeTorque = 500000;
-                    // If the player isn't inputting, we slow down quickly
-                    Debug.Log("Stopping");
-                } else {
-                    
-                    axleInfo.leftWheel.brakeTorque = 0;
-                    axleInfo.rightWheel.brakeTorque = 0;
-                }
-            }
+			if (enableFastStop) {
+				if (yAxis == 0) {
 
-            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
-            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
-        }
-    }
+					axleInfo.leftWheel.brakeTorque = 500000;
+					axleInfo.rightWheel.brakeTorque = 500000;
+					// If the player isn't inputting, we slow down quickly
+					Debug.Log("Stopping");
+				} else {
+
+					axleInfo.leftWheel.brakeTorque = 0;
+					axleInfo.rightWheel.brakeTorque = 0;
+				}
+			}
+
+			ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+			ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+		}
+	}
 
 	void OnCollisionEnter(Collision collision) {
 		if (collision.gameObject.tag == "obstacle") {
@@ -107,21 +102,4 @@ public class RVController : MonoBehaviour {
 			//TODO mess with PP propriotnoal to driving badness
 		}
 	}
-
-    void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag == "Goal") {
-            Debug.Log("You did it!");
-            controlEnabled = false;
-            StartCoroutine(FinishLevel());
-        }
-    }
-
-    IEnumerator FinishLevel() {
-
-        WinPanel winPanelInst = GameObject.Instantiate(winPanel, GameObject.Find("Canvas").transform);
-        winPanelInst.MovePanelOntoScreen();
-        
-        yield return new WaitForSeconds(2);
-        SceneManager.LoadScene("Main");
-    }
 }
