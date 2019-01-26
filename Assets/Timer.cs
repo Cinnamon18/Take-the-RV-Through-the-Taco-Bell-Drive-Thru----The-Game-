f7c012ec6c64ba34f990d4638a954beb34644d6e
WinaGodwin Anyanwu <w.anyanwu@live.com>
2019-01-26 03:49:34 -0500

    Game Over screen implemented

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -7,23 +7,32 @@ public class Timer : MonoBehaviour
 {
 	public Text timerTxt;
 	public float timeLeft;
-   
-    void Start()
+	
+	
+	void Start()
     {
 		StartCoroutine("CountDown");
 		Time.timeScale = 1;
-    }
+	}
 
     // Update is called once per frame
     void Update()
     {
 		float t = timeLeft;
 
+
 		string min = ((int)t / 60).ToString();
 		string sec = (t % 60).ToString("00");
 
 		timerTxt.text = min + ":" + sec;
-    }
+		if (timeLeft <= 0)
+		{
+			GameObject pUI = GameObject.Find("Player HUD");
+			GameOver gOver = pUI.GetComponent<GameOver>();
+			gOver.EndState();
+			//Application.Quit();
+		}
+	}
 
 	IEnumerator CountDown()
 	{
