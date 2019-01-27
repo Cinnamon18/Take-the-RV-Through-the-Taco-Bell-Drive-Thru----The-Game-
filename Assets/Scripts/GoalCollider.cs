5e44157fed1a33c2f132c4c3cc13b6309c82613b
WinaGodwin Anyanwu <w.anyanwu@live.com>
2019-01-26 20:10:37 -0500

    GoalCollider doesn't transition yet.

--- Assets/Scripts/GoalCollider.cs
+++ Assets/Scripts/GoalCollider.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GoalCollider : MonoBehaviour
@@ -13,9 +14,12 @@ public class GoalCollider : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void OnCollision (Collider hitObj)
     {
-
+		if (hitObj.gameObject.tag.Equals("Player") == true)
+		{
+			SceneManager.LoadScene("Main");
+		}
 	}
 
     private void OnTriggerEnter(Collider other)
