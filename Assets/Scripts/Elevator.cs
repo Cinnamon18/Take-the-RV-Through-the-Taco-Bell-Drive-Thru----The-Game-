4f4bb2d6c028d208c322ab91d791e8d9a25749ce
WinaGodwin Anyanwu <w.anyanwu@live.com>
2019-01-27 11:13:46 -0500

    Elevator script functional.

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -11,6 +11,9 @@ public class Elevator : MonoBehaviour
 	public int yMag;	
 	public Vector2 zMinMax;
 	public int zMag;
+	private int xHat = 1;
+	private int yHat = 1;
+	private int zHat = 1;
 
 
 	void Start()
@@ -21,35 +24,38 @@ public class Elevator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (transform.position.x <= xMinMax.x)
 		{
-			Translate(Vector3.right, xMag);
+			xHat = 1;
 		} else if (transform.position.x >= xMinMax.y)
 		{
-			Translate(Vector3.left, xMag);
+			xHat = -1;
 		}
 
 		if (transform.position.y <= yMinMax.x)
 		{
-			Translate(Vector3.up, yMag);
+			yHat = 1;
 		}
 		else if (transform.position.y >= yMinMax.y)
 		{
-			Translate(Vector3.down, yMag);
+			yHat = -1;
 		}
 
 		if (transform.position.z <= zMinMax.x)
 		{
-			Translate(Vector3.forward, yMag);
+			zHat = 1;
 		}
 		else if (transform.position.z >= yMinMax.y)
 		{
-			Translate(Vector3.back, yMag);
+			zHat = -1;
 		}
+
+		Translate();
 	}
 
-	private void Translate(Vector3 vHat, int mag)
+	private void Translate()
 	{
-		transform.Translate(vHat * Time.deltaTime * mag);
+		transform.Translate(xHat * Time.deltaTime * xMag, yHat * Time.deltaTime * yMag, zHat * Time.deltaTime * zMag);
 	}
 }
