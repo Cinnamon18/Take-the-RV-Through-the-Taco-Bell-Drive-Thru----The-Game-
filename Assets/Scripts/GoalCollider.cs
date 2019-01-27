73f0dac359af30dd391b64d59c72ac724fbe3a41
Cinnamon18 <alice.ava.moh@gmail.com>
2019-01-26 22:26:33 -0500

    got rid of game manager

--- Assets/Scripts/GoalCollider.cs
+++ Assets/Scripts/GoalCollider.cs
@@ -1,16 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GoalCollider : MonoBehaviour {
-
-	public bool hitPlayer;
-
 	// Update is called once per frame
 	void OnTriggerEnter(Collider hitObj) {
 		if (hitObj.gameObject.tag == "Player") {
-			hitPlayer = true;
+			StartCoroutine(SceneTransition.LoadScene(SceneManager.GetActiveScene().buildIndex));
 		}
 	}
 
