2b694bd69a648c22c75475cf2f801d790aa6464b
Speedwagon13 <kperras3@gatech.edu>
2019-01-27 10:53:31 -0500

    updated rv graveyard

--- Assets/RV_RandomColor.cs
+++ Assets/RV_RandomColor.cs
@@ -10,7 +10,14 @@ public class RV_RandomColor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rvMat = transform.GetChild(0).GetComponent<Material>();
-        Color rvColor = colors[Random.Range(0, 6)];
+        rvMat = transform.GetChild(0).GetComponent<MeshRenderer>().material;
+        Color rvColor = colors[Random.Range(0, 7)];
+        rvMat.color = rvColor;
+
+        // random rotation
+        if (Random.Range(0f, 1f) < .5f)
+            transform.forward *= -1f;
+
+        transform.Rotate(0, Random.Range(-2f, 2f), 0);
     }
 }
